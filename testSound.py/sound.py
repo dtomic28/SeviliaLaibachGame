2ac9decd9d99c99625fc3b614961a6ae40d13443
zanoixo <62448782+zanoixo@users.noreply.github.com>
2022-02-24 19:53:12 +0100

    Sound thingi worki worki
    Sound thingi worki worki

--- testSound.py/sound.py
+++ testSound.py/sound.py
@@ -1,3 +1,4 @@
+from pygame_menu import Sound
 import scipy.io.wavfile as wavfile
 import scipy
 import scipy.fftpack as fftpk
@@ -5,34 +6,30 @@ import numpy as np
 from matplotlib import pyplot as plt
 import soundfile as sf
 import pyloudnorm as pyln
-
-s_rate, signal = wavfile.read(r"C:\Users\majar\Documents\GitHub\SeviliaLaibachGame\testSound.py\test.wav")
-FFT = signal
-freqs=fftpk.fftfreq(len(FFT),(1.0/s_rate))
-st=0
-timer=0
-min=0
-max=10000000
-
-with open("datoteka.txt","w") as f:
+def soundCheck():
+    s_rate, signal = wavfile.read(r"C:\Users\majar\Documents\GitHub\SeviliaLaibachGame\testSound.py\file.wav")
+    FFT = signal
+    freqs=fftpk.fftfreq(len(FFT),(1.0/s_rate))
+    st=0
+    timer=0
+    min=0
+    max=0
+    SoundWaveSez=[]
     for i in range(0,len(signal),s_rate//10):
+        if(int(signal[i])>max):
+            max=int(signal[i])
+        if(int(signal[i])<min):
+            min = int(signal[i])
         print(str(signal[i])+" "+str(timer)+": s")
         st=(st+1)%10
         if(st==9):
             timer+=1
-        f.write(str(signal[i]))
+        SoundWaveSez.append(int(signal[i]))
 
-
-data, rate = sf.read(r"C:\Users\majar\Documents\GitHub\SeviliaLaibachGame\testSound.py\test.wav")
-meter = pyln.Meter(rate) 
-print(data[10000][0])
-"""
-for i in range(0,len(data),):
-    loudness = meter.integrated_loudness(data[i])
-    print(loudness)
-
-"""
-plt.plot(freqs[range(len(FFT))],FFT[range(len(FFT))])
-plt.xlabel("frequency (Hz)")
-plt.ylabel("Amplitude")
-plt.show()
+    SpawnObject=[]
+    for i in SoundWaveSez:
+        SpawnObject.append(((i+abs(min))/(max+abs(min))*5)//1)
+    print(SpawnObject)
+    print(min)
+    print(max)
+soundCheck()
