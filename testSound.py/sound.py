ea8186fa604024100db98388bdabac1c5d51ca63
zanoixo <62448782+zanoixo@users.noreply.github.com>
2022-02-24 12:32:37 +0100

    sound
    sound

--- testSound.py/sound.py
+++ testSound.py/sound.py
@@ -3,3 +3,20 @@ import scipy
 import scipy.fftpack as fftpk
 import numpy as np
 from matplotlib import pyplot as plt
+import pydub
+
+
+s_rate, signal = wavfile.read(r"C:\Users\majar\Documents\GitHub\SeviliaLaibachGame\testSound.py\file.wav")
+FFT = signal
+freqs=fftpk.fftfreq(len(FFT),(1.0/s_rate))
+st=0
+with open("datoteka.txt","w") as f:
+    for i in range(0,len(signal),8000):
+        st+=1
+        print(signal[i])
+        f.write(str(signal[i]))
+print(st)
+plt.plot(freqs[range(len(FFT))],FFT[range(len(FFT))])
+plt.xlabel("frequency (Hz)")
+plt.ylabel("Amplitude")
+plt.show()
